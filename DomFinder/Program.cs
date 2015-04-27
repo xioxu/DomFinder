ebb325bd0edf18d7e3d8569c6ceb4a40d276b0fb
xioxu <xioxu@aa.com>
2015-04-27 13:58:14 +0800

    init

---
+++ DomFinder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using DomFinderLibrary;
+
+namespace DomFinderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var href = "http://quote.eastmoney.com/sz000599.html";
+
+            WebClient client = new WebClient();
+
+            var htmlContent = client.DownloadString(href);
+
+          //  var table = new DomFinder(htmlContent).Find.ByExpression<HtmlTable>("id=wahaha", "|", "tagIndex=table:0");
+
+            var table = new DomFinder(htmlContent).Find.ByExpression<HtmlTable>("id=rtp2");
+
+            if (table != null)
+            {
+                foreach (var row in table.BodyRows)
+                {
+                    foreach (var cell in row.Cells)
+                    {
+                        Console.Write(cell.InnerText + "   ");
+                    }
+                }
+
+                Console.WriteLine("\r\n-------------------------------");
+            }
+
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+    }
+}
---
+++ DomFinderLibrary/DomFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.ObjectModel;
+
+namespace DomFinderLibrary
+{
+    public class DomFinder
+    {
+        private Find _find;
+
+        public DomFinder(string htmlText)
+        {
+            HtmlTreeBuilder treeBuilder = new HtmlTreeBuilder();
+            var domRoot = treeBuilder.BuildDomTree(htmlText, true);
+
+            BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            _find = Activator.CreateInstance(typeof(Find), flags, null, new object[] { domRoot }, null) as Find;
+        }
+
+        public Find Find
+        {
+            get { return _find; }
+        }
+    }
+}
